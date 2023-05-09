07b9a4c748ec14f1e46c4f3ae327df70bc2504df
KSVQ <kristinasaricim@gmail.com>
2023-05-09 19:56:20 -0400

    Added Files

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 def hello_world():
     return render_template("index.html")
 
+@app.route("/settings")
+def settings():
+    return render_template("settings.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
