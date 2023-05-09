58d548edcf28b9185960af4abb532b0f2810daba
KSVQ <kristinasaricim@gmail.com>
2023-05-09 19:41:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-
+# Disable caching
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
