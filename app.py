b6ddce525ae39aa1cd1733e92efcd93c736d800c
KSVQ <kristinasaricim@gmail.com>
2023-05-10 07:46:34 -0400

    Interface progress

--- app.py
+++ app.py
@@ -14,9 +14,20 @@ def settings():
     return render_template("settings.html")
 
 @app.route("/account")
-def saccount():
+def account():
     return render_template("account.html")
 
+@app.route("/faq")
+def faq():
+    return render_template("faq.html")
+
+@app.route("/pricing")
+def pricing():
+    return render_template("pricing.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
